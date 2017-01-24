31a6d8d7dd9d2dfbe919354eb3c3c7d808769b54
ls <indians4german@gmail.com>
2017-01-24 18:02:08 +0530

    enabled sessions var MasonaryObj = [];

--- app/questionController.js
+++ app/questionController.js
@@ -3,12 +3,9 @@ var question = require("../models/question.js");
 module.exports = {
   index: function (req, res) {   
 
-  	var selectedBuilding = 'Masonry'; //req.session["BuildingName"] ;  
+  	var selectedBuilding =   req.session.BuildingName;  
       question.find({'buildingsAssociated._id': selectedBuilding}, function(err, data){  
-        var Language =  'English'; //req.session.BuildingName ;                  
-        for(var i =0;i< data.length ; i ++) {
-        	console.log(data[i]);
-        };
+        var Language =  req.session.languageSelected ;                          
         res.render('general_Info-Form',{ rawData: data , Language : Language});
       });        
   }  
