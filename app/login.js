4daea5b336675ae1499706619d844bb8c866178d
ls <indians4german@gmail.com>
2017-02-08 20:14:54 +0530

    no to master

--- app/login.js
+++ app/login.js
@@ -1,32 +1,77 @@
-var user = require("../models/user.js");
+var User = require("../models/user.js");
+///var SignupUser = require("../models/signup_user.js");
 var bcrypt = require('bcrypt');
 
 module.exports = {
+	/*
   index: function (req, res) { 
- 
-  	user.findOne({name:"user"}, function(err, data){  		
-  		var salt = bcrypt.genSaltSync(10);// Hash the password with the salt
-		var hash = bcrypt.hashSync("papa", salt);		 
-		if(req.body.email == "user@gmail.com"){ 
-			data.comparePassword(req.body.password, function (err, isMatch) {
-			if (isMatch && !err) {
+   var info = req.body;
+    
+	user.find({email: req.body.email}, function(err, data){  
+		//console.log(data[0].email)
+		if(req.body.email == data[0].email){     //console.log(data);
+			data[0].comparePassword(req.body.password, function (err, isMatch) {//console.log(data[0].password);
+			if (isMatch && !err) { 
 				//store whatever you need to store in the locals or session
-				sess = req.session;
-				sess.user = req.body.email;			        
+				//sess = req.session;
+				//sess.user = req.body.email;	
+				
+				             req.session.email = data[0].email;	        
 				res.redirect('/2');
 			  }
 			else{ console.log('false');res.send('Wrong password'); }
 			});		
 		} else { res.redirect('/'); };
-  	});           
-  	  
-  },
+  	}); 
+  },*/
+  index :  function (req, res){
+		var data = req.body;
+		console.log(data.password);
+		User.findOne({email : data.email}).exec(function (err, val){
+			if(val=="" || val==null){
+				return res.send("No such user exists");
+			}
+			if(err){
+				console.log("error");
+			}
+            
+				val.comparePassword(data.password, function (err, isMatch){
+                 if(isMatch && !err){
+                 	//handle sessions here
+                 req.session.email = val.email;
+                 req.session.name = val.name;
+                 req.session.password = val.password;
+                 	
+                 	console.log("Session here:-"+req.session.name);
+                 	      return res.json({error : false, name : req.session.name});
+                 }
+                 
+                 	res.send("password mismatch");
+                 	
+		        });
 
+        });
+    
+    },
 
-  Saltify : function(req,res){
-  	    var salt = bcrypt.genSaltSync(10);// Hash the password with the salt
-  		var hash = bcrypt.hashSync("user", salt);
-  		res.send(hash);
+  adduser: function (req, res) {
+  	var info = req.body;
+  	console.log(info);
+  	var query = User(info);
+		query.save(function (err, val){
+			if(err){
+				res.send("error");
+				console.log("error");
+			}
+			else{
+				console.log("you submitted-----" + val);
+			}
+		});
+	
+	   res.send('user signed up');
+	  // res.render('login');
+  	
   }
+
 
 }  
---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'development': {
+    'secret': 'bolbona',
+    'database': 'mongodb://test:test@ds157248.mlab.com:57248/rv',
+    //'database': 'mongodb://localhost:27017/donatemytime',
+    'apiPath' : '/api/v1',
+    'siteUrl': 'http://localhost:3000'
+  },
+  'staging': {
+    'secret': '059a53cab2f21hjj1e6b53ad7182f354492',
+    //'database': 'mongodb://localhost:27017/donatemytime',
+    'database' : 'mongodb://test:test@ds157248.mlab.com:57248/rv',
+    'apiPath' : '/api/v1',
+    'siteUrl': 'http://139.59.34.61:3000'
+  }
+};
